baed49bc82349320c6e465deac64b78a58621eda
dineshbhuykya54 <dineshbhukya5040@gmail.com>
2026-03-26 20:15:32 +0530

    updated files

---
+++ Primitives.java
@@ -0,0 +1,14 @@
+
+public class Primitives {
+
+    public static void main(String[] args) {
+        // Data types:
+        //    int rollno = 54;
+        //    char letter = 'r';
+        //    float marks = 95.54f;
+        //    double largeDecimalNumber = 5309457022.39573;
+        //    long largeInteger = 57309457820394820L;
+        //    boolean check = true/false;
+    }
+
+}
---
+++ input.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class input {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter input");
+        String name = input.next();
+        System.out.println(" your name is " + name);
+        char letter = 'd';
+        System.out.println("letter is " + letter);
+        int num = 54;
+        System.out.println("number is " + num);
+        float marks = 95.54f;
+        System.out.println("your marks is " + marks);
+        double DecNum = 39875203947.54;
+        System.out.println("your Dec Num is " + DecNum);
+        long intger = 29384702478209L;
+        System.out.println("your integer is " + intger);
+
+    }
+}
