1a22ac3544baa00c61abcfb71ceee5f8a760d950
dineshbhuykya54 <dineshbhukya5040@gmail.com>
2026-03-26 20:41:07 +0530

    new file

--- input.java
+++ input.java
@@ -6,11 +6,11 @@ public class input {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("enter input");
-        String name = input.next();
-        System.out.println(" your name is " + name);
-        char letter = 'd';
-        System.out.println("letter is " + letter);
-        int num = 54;
+        // String name = input.nextLine();
+        // System.out.println(" your name is " + name);
+        // char letter = 'd';
+        // System.out.println("letter is " + letter);
+        int num = input.nextInt();
         System.out.println("number is " + num);
         float marks = 95.54f;
         System.out.println("your marks is " + marks);
@@ -18,6 +18,11 @@ public class input {
         System.out.println("your Dec Num is " + DecNum);
         long integer = 29384702478209L;
         System.out.println("your integer is " + integer);
+        // sum of two numbers:
+        int a =10;
+        int b =20;
+        int sum = a + b;
+        System.out.println("your sum is " + sum);
 
     }
 }
